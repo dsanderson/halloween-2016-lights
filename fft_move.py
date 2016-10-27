53fa7fb22293191e6ec6c0af3b5bc4ec20e18d75
dsa@rpi <dsa@rpi>
2016-10-28 00:06:10 +0800

    final version

--- fft_move.py
+++ fft_move.py
@@ -4,27 +4,37 @@ import colorsys, time, sys, random, copy
 def update_colors(spectrogram,vis,fps, i1, i2, old_cols):
     current_hues = [colorsys.rgb_to_hsv(*s)[0] for s in old_cols]
     #scale = 11796480000.0/(2*fps)#2*2^16
-    ni = min([8,int(sum(spectrogram))/2])
+    thresh = min([1,sum(spectrogram)/8.0])
+    thresh = thresh/fps
     if not disp:
-		print ni
-    ids = random.sample(range(8),ni)
+        print thresh
+    if random.random()<thresh:
+        i1 += random.choice([-1,1])
+        i1 = i1%8
+    if random.random()<thresh:
+        i2 += random.choice([-1,1])
+        i2=i2%8
+    if not disp:
+        print i1,i2
     scale = (2*fps)
     scale_i = [0.2,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
     vel = [scale_i[i]*s*(i+1)/float(scale) for i, s in enumerate(spectrogram)]
-    if not disp:
-        print spectrogram
+    #if not disp:
+        #print spectrogram
     new_hues = [(h+vel[i])%1.0 for i, h in enumerate(current_hues)]
     new_colors = [colorsys.hsv_to_rgb(h,1.0,1.0) for h in new_hues]
     old_cols = copy.deepcopy(new_colors)
     for i in xrange(len(new_colors)):
-		if i not in ids:
-			new_colors[i] = (0.0,0.0,0.0)
+        if i not in [i1,i2]:
+            new_colors[i] = (0.0,0.0,0.0)
     vis.state = new_colors
+    if not disp:
+        print new_colors
     vis.write_state()
     return scale, i1, i2, old_cols
 
 if __name__ == '__main__':
-    disp = False
+    disp = True
     vis = comms.Visualizer(debug=False)
     rec = audio.Recorder()
     fps = 106.0
@@ -56,3 +66,4 @@ if __name__ == '__main__':
         #    time.sleep(spf-time_diff)
         fps = 1.0/time_diff
         #time_0 = time_1
+
